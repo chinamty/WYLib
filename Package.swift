233123eb11b419b9a6c6a9c40889adc6eac162ad
Martin <tingyuanma@gmail.com>
2023-10-26 09:18:24 +0800

    add framework

--- Package.swift
+++ Package.swift
@@ -8,16 +8,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "WYLib",
-            targets: ["WYLib"]),
+            name: "WYBOTFramework",
+            targets: ["WYBOTFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "WYLib"),
-        .testTarget(
-            name: "WYLibTests",
-            dependencies: ["WYLib"]),
+			.binaryTarget(name: "WYBOTFramework", path: "./Sources/WYBOTFramework.xcframework")
     ]
 )
---
+++ Sources/WYBOTFramework.xcframework/ios-arm64/WYBOTFramework.framework/Headers/WYBOTFramework.h
@@ -0,0 +1,18 @@
+//
+//  WYBOTFramework.h
+//  WYBOTFramework
+//
+//  Created by 马庭元 on 2023/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WYBOTFramework.
+FOUNDATION_EXPORT double WYBOTFrameworkVersionNumber;
+
+//! Project version string for WYBOTFramework.
+FOUNDATION_EXPORT const unsigned char WYBOTFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WYBOTFramework/PublicHeader.h>
+
+
